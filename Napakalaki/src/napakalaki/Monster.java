466ae1dc52fe29491875ab0aaa3348cf1e265ece
jojelupipa <jesus_38b@hotmail.com>
2017-03-02 13:09:14 +0100

    Añadida la clase Monster con sus correspondientes métodos

---
+++ Napakalaki/src/napakalaki/Monster.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author Miguel Robles Urquiza & Jesús Sánchez de Lechina Tejada
+ */
+public class Monster {
+    private String name;
+    private int combatLevel;
+
+    private Prize prize;
+    private BadConsequence badconsequence;
+    
+    public Monster(String name, int level, Prize prize, BadConsequence bc){
+        this.name = name;
+        this.combatLevel = level;
+        this.prize = prize;
+        this.badconsequence = bc;
+    }
+    
+    public String getString(){
+        return name;
+    }
+    
+    public int getCombatLevel(){
+        return combatLevel;
+    }
+    
+    public Prize getPrize(){
+        return prize;
+    }
+    
+    public BadConsequence getBadConsequence(){
+        return badconsequence;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public void setCombatLevel(int level){
+        this.combatLevel = level;
+    }
+    
+    public void setPrize(Prize prize){
+        this.prize = prize;
+    }
+    
+    public void setBadConsequence(BadConsequence bc){
+        this.badconsequence = bc;
+    }
+
+}
+    

38d3663b7f057658bd5f1655935191b3b405f6f7
jojelupipa <jesus_38b@hotmail.com>
2017-03-05 23:43:07 +0100

    Rubykalaki: Añadidos módulos y clases TreasureKind y BadConsequence. Esta última en construcción

--- NapakalakiRuby/lib/prize.rb
+++ NapakalakiRuby/lib/prize.rb
@@ -2,16 +2,13 @@
 # By: Miguel Robles Urquiza
 #     Jesús Sánchez de Lechina Tejada
 class Prize
-  #Alternativamente a los set/get se puede usar "attr_accessor :treasures,:levels"
-  
+  attr_accessor :treasures, :levels # Alternativamente se pueden usar métodos set/get 
+ 
   def initialize(treasures, levels)
     @treasures = treasures
     @levels = levels
   end
   
-  def getTreasures
-    @treasures
-  end
   
 end
 

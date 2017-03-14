6acedaddd4346634750af0a7f7cf8530023761bc
MigueRobles <miguerobles@correo.ugr.es>
2017-03-14 17:34:07 +0100

    Aniadidas dependencias

--- NapakalakiRuby/lib/pruebaNapakalaki.rb
+++ NapakalakiRuby/lib/pruebaNapakalaki.rb
@@ -1,6 +1,10 @@
 # By: Miguel Robles Urquiza
 #     Jesús Sánchez de Lechina Tejada
 
+require_relative "prize.rb"
+require_relative "monster.rb"
+#encoding: utf-8
+
 class Napakalaki
   # Definimos array para los monstruos
   monstruos = Array.new()

56ea259d788147cb0c7dd175b204b54f66aacdfe
MigueRobles <miguerobles@correo.ugr.es>
2017-03-21 00:36:39 +0100

    Clase terminada

--- NapakalakiRuby/lib/treasure.rb
+++ NapakalakiRuby/lib/treasure.rb
@@ -1,8 +1,33 @@
 # By: Miguel Robles Urquiza
 #     Jesús Sánchez de Lechina Tejada
 
+#encoding: utf-8
+
+require_relative "treasure_kind"
+
+
 class Treasure
-  def initialize
+  
+  def initialize (n, bonus, t)
+     @name = n
+     @bonus = bonus
+     @type = t
+    end
+    def getName()
+      @name
+  
+    end
+        
+    def getBonus
+      @bonus
+    end
     
-  end
+    def getType
+      @type
+    end
+    
+    def to_s
+      "Treasure: #{@name} Type: #{@type} Bonus:{@bonus}\n" 
+    end
+    
 end

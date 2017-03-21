6b13707fd895961f247fc1fc97b797efe57fdcf9
MigueRobles <miguerobles@correo.ugr.es>
2017-03-21 17:59:34 +0100

    Acabada clase Card_Dealer P1

--- NapakalakiRuby/lib/card_dealer.rb
+++ NapakalakiRuby/lib/card_dealer.rb
@@ -6,8 +6,7 @@ module NapakalakiGame
 
   class Card_dealer
     
-    include Singleton
-    
+    include Singleton    
     
     def initialize
       
@@ -21,65 +20,35 @@ module NapakalakiGame
     def initTreasureCardDeck()      
 
       @unusedTreasures << Treasure.new("¡Sí mi amo!", 4, [TreasureKind::HELMET])
-
       @unusedTreasures << Treasure.new("Botas de investigación", 3, [TreasureKind::SHOES])
-
       @unusedTreasures << Treasure.new("Capucha de Cthulhu", 3, [TreasureKind::HELMET])
-
       @unusedTreasures << Treasure.new("A prueba de babas", 2, [TreasureKind::ARMOR])
-
       @unusedTreasures << Treasure.new("Botas de lluvia ácida", 1, [TreasureKind::BOTHHANDS])
-
       @unusedTreasures << Treasure.new("Casco minero", 2, [TreasureKind::HELMET])
-
       @unusedTreasures << Treasure.new("Ametralladora ACME", 4, [TreasureKind::BOTHHANDS])
-
       @unusedTreasures << Treasure.new("Camiseta de la ETSIIT", 1, [TreasureKind::ARMOR])
-
       @unusedTreasures << Treasure.new("Clavo de rail ferroviario", 3, [TreasureKind::ONEHAND])
-
       @unusedTreasures << Treasure.new("Cuchillo de sushi arcano", 2, [TreasureKind::ONEHAND])
-
       @unusedTreasures << Treasure.new("Fez alópodo", 3, [TreasureKind::HELMET])
-
       @unusedTreasures << Treasure.new("Hacha prehistorica", 2, [TreasureKind::ONEHAND])
-
       @unusedTreasures << Treasure.new("El aparato del Pr. Tesla", 4, [TreasureKind::ARMOR])
-
       @unusedTreasures << Treasure.new("Gaita", 4, [TreasureKind::BOTHHANDS])
-
       @unusedTreasures << Treasure.new("Insecticida", 2, [TreasureKind::ONEHAND])
-
       @unusedTreasures << Treasure.new("Escopeta de 3 cañones", 4, [TreasureKind::BOTHHANDS])
-
       @unusedTreasures << Treasure.new("Garabato mistico", 2, [TreasureKind::ONEHAND])
-
       @unusedTreasures << Treasure.new("La rebeca metálica", 2, [TreasureKind::ARMOR])
-
       @unusedTreasures << Treasure.new("Lanzallamas", 4, [TreasureKind::BOTHHANDS])
-
       @unusedTreasures << Treasure.new("Necro-comicón", 1, [TreasureKind::ONEHAND])
-
       @unusedTreasures << Treasure.new("Necronomicón", 5, [TreasureKind::BOTHHANDS])
-
       @unusedTreasures << Treasure.new("Linterna a 2 manos", 3, [TreasureKind::BOTHHANDS])
-
       @unusedTreasures << Treasure.new("Necro-gnomicón", 2, [TreasureKind::ONEHAND])
-
       @unusedTreasures << Treasure.new("Necrotelecom", 2, [TreasureKind::HELMET])
-
       @unusedTreasures << Treasure.new("Mazo de los antiguos", 3, [TreasureKind::ONEHAND])
-
       @unusedTreasures << Treasure.new("Necro-playboycón", 3, [TreasureKind::ONEHAND])
-
       @unusedTreasures << Treasure.new("Porra preternatural", 2, [TreasureKind::ONEHAND])
-
       @unusedTreasures << Treasure.new("Shogulador", 1, [TreasureKind::BOTHHANDS])
-
       @unusedTreasures << Treasure.new("Varita de atizamiento", 3, [TreasureKind::ONEHAND])
-
       @unusedTreasures << Treasure.new("Tentáculo de pega", 2, [TreasureKind::HELMET])
-
       @unusedTreasures << Treasure.new("Zapato deja-amigos", 1, [TreasureKind::SHOES])
 
     end
@@ -179,7 +148,7 @@ module NapakalakiGame
     def giveMonsterBack(m)
        @usedMonsters << m
     end 
-
+=begin
     def nextTreasure()
 
     end
@@ -191,11 +160,7 @@ module NapakalakiGame
     def initCards()
 
     end
-    
-    def getInstance()
-      
-    end
-
+=end  
   end
 
 end

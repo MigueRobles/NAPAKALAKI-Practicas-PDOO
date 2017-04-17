623abb02b8b41dd7f44422f3e66c3b6d6804b3eb
MigueRobles <miguerobles@correo.ugr.es>
2017-04-17 12:57:20 +0200

    Ruby: Terminada implementacion clase Player

--- NapakalakiRuby/lib/player.rb
+++ NapakalakiRuby/lib/player.rb
@@ -36,12 +36,20 @@ module NapakalakiGame
       return clevel
     end
 
-    def incrementLevels(i)#ver si nivel esta entre 0 y 10 y que no se quede negativo
-      @level += i    
+    def incrementLevels(i)
+      if ((@level+i)>10)
+        @level = 10
+      else
+        @level += i
+      end    
     end
 
     def decrementLevels(i)
-      @level -= i
+      if (i>@level)
+        @level = 1
+      else
+        @level -= i
+      end
     end
 
     def setPendingBadConsequence(b)
@@ -49,22 +57,22 @@ module NapakalakiGame
     end
     
     def applyPrize(m)
-      @level += m.levels
-      m.treasure.each do |tesoro| 
-        @hiddenTreasures << tesoro
+      incrementLevels(m.getLevelsGained)
+      if (m.getTreasuresGained > 0)
+        dealer = CardDealer.instance
+        m.getTreasuresGained.times do 
+          treasure = dealer.nextTreasure
+          @hiddenTreasures << treasure
+        end
       end
     end
 
     def applyBadConsequence(b)
-      if(b.death == true)
-        @levels = 1
-        @hiddenTreasures.clear()
-        @visibleTreasures.clear()
-        @pendingBadConsequence.clear() # Para comprobar el estado válido vemos si no tiene ningún mal rollo por cumplir
-            
-      else 
-        @levels -= b.levels
-      end
+      badConsequence = m.getBadconsequence
+      nLevels = badConsequence.getLevels
+      decrementLevels(nLevels)
+      pendingBad = badConsequence.adjustToFitTreasureLists(@visibleTreasures, @hiddenTreasures)
+      setPendingBadConsequence(pendingBad)
     end   
     
     def howManyVisibleTreasures(tKind)
@@ -97,7 +105,7 @@ module NapakalakiGame
 
     def validState
       return true if (@pendingBadConsequence == nil)
-      return true if (@pendingBadConsequence.isEmpty() == true && @hiddenTreasures.length < 5)
+      return true if (@pendingBadConsequence.isEmpty == true && @hiddenTreasures.length < 5)
       
       return false
     end
@@ -123,40 +131,143 @@ module NapakalakiGame
       @canISteal = false
     end
 
-    def canMakeTreasureVisible(t)
-
+   def canMakeTreasureVisible(t)
+      
+      if (t.getType == TreasureKind::ONEHAND)         # Para una mano
+        if (treasureInUse(TreasureKind::BOTHHANDS))   # Si tenemos equipada una de dos manos
+          return false
+        else                                          # Si no tenemos de dos manos, ¿Cuántas de 1 mano?
+          una_mano = 0
+          @visibleTreasures.each do |i|
+            if (TreasureKind::ONEHAND == i.getType)
+              una_mano += 1
+            end
+          end
+          if (una_mano >= 2)                          # Si tenemos 2 o más -> No
+            return false
+          end
+        end
+        
+      else
+        if (t.getType == TreasureKind::BOTHHANDS)     # Para dos manos
+          if (treasureInUse(TreasureKind::BOTHHANDS) || treasureInUse(TreasureKind::ONEHAND))
+            return false
+          end
+        else
+          if (treasureInUse(t.getType))               # Resto del tipo de tesoros
+            return false
+          end
+        end
+      end
+      return true
+    end
+        
+    def treasureInUse (type)
+      @visibleTreasures.each do |i|
+        if(type == i.getType)
+          return true
+        end
+      end
+      return false
     end
 
-    def combat(m)
-
+   def combat(m)
+      myLevel = getCombatLevel
+      monsterLevel = m.getCombatLevel
+      if (@canISteal == false)
+        dice = Dice.instance
+        number = dice.nextNumber
+        if (number < 3)
+          enemyLevel = @enemy.getCombatLevel
+          monsterLevel += enemyLevel
+        end
+      end
+      if (myLevel > monsterLevel)
+        applyPrize(m)
+        if (@level >= 10)
+          combatResult = CombatResult::WINGAME
+        else
+          combatResult = CombatResult::WIN
+        end
+      else
+        applyBadConsequence(m)
+        combatResult = CombatResult::LOSE
+      end
+      return combatResult
+    end
+    
+   def makeTreasureVisible(t)
+      canI = canMakeTreasureVisible(t)
+      if (canI)
+        @visibleTreasures << t
+        @hiddenTreasures.delete_at(@hiddenTreasures.index(t)||@hiddenTreasures.length)
+      end
     end
 
-    def makeTreasureVisible(t)
-
-    end
-
-    def discardVisibleTreasure(t)
-
+        def discardVisibleTreasure(t)
+      @visibleTreasures.delete_at(@visibleTreasures.index(t)||@hiddenTreasures.length)
+      if (@pendingBadConsequence != nil && @pendingBadConsequence.isEmpty() == false)
+        @pendingBadConsequence.substractVisibleTreasure(t)
+      end
+      
+      dieIfNoTreasures()
     end
 
     def discardHiddenTreasure(t)
-
+      @hiddenTreasures.delete_at(@hiddenTreasures.index(t) || @hiddenTreasures.length)
+      if (@pendingBadConsequence != nil)
+        if(@pendingBadConsequence.isEmpty() == false)
+          @pendingBadConsequence.substractHiddenTreasure(t)
+        end
+      end
+      dieIfNoTreasures
     end
 
     def initTreasures
-
+      dealer = CardDealer.instance
+      dice = Dice.instance
+      bringToLife
+      treasure = dealer.nextTreasure
+      @hiddenTreasures << treasure
+      number = dice.nextNumber
+      if (number > 1)
+        treasure = dealer.nextTreasure
+        @hiddenTreasures << treasure        
+      end
+      if (number == 6)
+        treasure = dealer.nextTreasure
+        @hiddenTreasures << treasure
+      end
     end
 
     def stealTreasure
-
+      canI = canISteal
+      if (canI)
+        if (@enemy.canYouGiveMeATreasure)
+          treasure = @enemy.giveMeATreasure
+          @hiddenTreasures << treasure
+          haveStolen
+        end
+      else
+        treasure = nil
+      end
+      return treasure
     end
 
     def giveMeATreasure
-
+      indice = rand(@hiddenTreasures.size)
+      return @hiddenTreasures[indice]
     end
 
     def discardAllTreasures
-
+      cVisible= Array.new(@visibleTreasures)
+      cHidden = Array.new(@hiddenTreasures)
+      cVisible.each do |treasure|
+        discardVisibleTreasure(treasure)
+      end
+      cHidden.each do |treasure|
+        discardHiddenTreasure(treasure)
+      end
     end
     
   private :bringToLife, :getCombatLevel, :incrementLevels, :decrementLevels, :setPendingBadConsequence, :applyPrize, :applyBadConsequence, :canMakeTreasureVisible, :howManyVisibleTreasures, :dieIfNoTreasures, :giveMeATreasure, :canYouGiveMeATreasure, :haveStolen

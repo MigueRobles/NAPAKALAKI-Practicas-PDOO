52415b82429bcf90c02a0b17bb08b49fd8dc40b9
jojelupipa <jesus_38b@hotmail.com>
2017-04-17 12:47:19 +0200

    Java: Class Napakalaki: Corregidos unos errores que aparecían en el build

--- Napakalaki/src/NapakalakiGame/Napakalaki.java
+++ Napakalaki/src/NapakalakiGame/Napakalaki.java
@@ -1,7 +1,6 @@
 package NapakalakiGame;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -34,12 +33,12 @@ public class Napakalaki {
     }
     
     private Player nextPlayer() {
-        if(currentPlayer.getName() == new String){        // Indica si aún no se ha elegido un jugador en el primer turno
-            turno = 1 + rand(@players.size)
-        
+        if(currentPlayer.getName() == new String())        // Indica si aún no se ha elegido un jugador en el primer turno
+            turno = 1 + (int) (Math.random() * (players.size()));
+             
         else 
             turno = (turno + 1) % players.size();
-        end
+        
         currentPlayer = players.get(turno);
         
         return currentPlayer;

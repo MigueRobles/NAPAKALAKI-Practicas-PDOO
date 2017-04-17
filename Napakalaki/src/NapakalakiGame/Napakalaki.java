048836e52a0e6f37acf9d6aec6c1f6081a32840c
MigueRobles <miguerobles@correo.ugr.es>
2017-04-17 12:14:45 +0200

    Java: Arreglo Napakalaki

--- Napakalaki/src/NapakalakiGame/Napakalaki.java
+++ Napakalaki/src/NapakalakiGame/Napakalaki.java
@@ -34,12 +34,12 @@ public class Napakalaki {
     }
     
     private Player nextPlayer() {
-        if(currentPlayer.getName() == new String){        // Indica si aún no se ha elegido un jugador en el primer turno
-            turno = 1 + rand(@players.size)
-        
+        if(currentPlayer.getName() == new String()){      
+        turno = (int) (Math.random() * (players.size()));
+        }
         else 
             turno = (turno + 1) % players.size();
-        end
+        
         currentPlayer = players.get(turno);
         
         return currentPlayer;
